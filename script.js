8c0869185b92fe6ef1834e491dec451639e6f3e7
aplusquick78 <97718240+aplusquick78@users.noreply.github.com>
2024-01-15 21:20:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,18 @@ function clearDisplay() {
     display.value = '';
 }
 
+function backspace() {
+    display.value = display.value.slice(0, -1);
+}
+
 function calculate() {
     try {
         display.value = eval(display.value);
     } catch (error) {
         display.value = 'Error';
     }
+}
+
+function percent() {
+    display.value = String(parseFloat(display.value) / 100);
 }
